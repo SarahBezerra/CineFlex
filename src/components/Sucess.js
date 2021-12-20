e2cca08cda6c3d21f7ddd2f20d01b8a96597c322
Sarah <sarahbezerra2013@gmail.com>
2021-12-20 18:10:21 -0300

    Layout da Tela de Sucesso

--- src/components/Sucess.js
+++ src/components/Sucess.js
@@ -19,21 +19,23 @@ export default function Sucess(props) {
     return(
         <>
         <Title>
-            Pedido feito com sucesso!
+            Pedido feito{"\n"}com sucesso!
         </Title>
 
-        <h2>Filme e sessão</h2>
-        <p></p>
-        <p></p>
+        <MovieInfo>
+            <h2>Filme e sessão</h2>
+            <p></p>
+            <p></p>
 
-        <h2>Ingressos</h2>
-        {props.dataSucess.seats.map(info =>
-            <p>{`Assento: ${info.name}`}</p>
-        )}
+            <h2>Ingressos</h2>
+            {props.dataSucess.seats.map(info =>
+                <p>{`Assento: ${info.name}`}</p>
+            )}
 
-        <h2>Comprador</h2>
-        <p>{`Nome: ${props.dataSucess.name}`}</p>
-        <p>{`CPF: ${props.dataSucess.cpf}`}</p>
+            <h2>Comprador</h2>
+            <p>{`Nome: ${props.dataSucess.name}`}</p>
+            <p>{`CPF: ${props.dataSucess.cpf}`}</p>
+        </MovieInfo>
 
         <Btn>
             <Link to={"/"}>
@@ -47,12 +49,39 @@ export default function Sucess(props) {
 
 const Title = styled.h1`
     font-size: 24px;
+    text-align: center;
+    font-weight: 700;
+    color: #247A6B;
+    line-height: 28px;
+    letter-spacing: 0.04em;
+
+    margin-top: 30px;
+`;
+
+const MovieInfo = styled.div`
+    h2{
+        font-size: 24px;
+        color: #293845;
+        line-height: 35px;
+        font-weight: 700;
+
+        margin-top: 30px;
+    }
+
+    p{
+        font-size: 22px;
+        color: #293845;
+        line-height: 26px;
+        font-weight: 400;
+    }
 `;
 
 const Btn = styled.div`
     width: 100%;
     display: flex;
     justify-content: center;
+
+    margin-top: 50px;
 
     div{
         width: 225px;
